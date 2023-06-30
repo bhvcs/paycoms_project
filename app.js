9455af0851eafa369ed124182aa5dd929bdbf5ae
2020036217 <2020036217@hanyang.ac.kr>
2023-06-30 15:02:44 +0900

    헤더 첫번째 연습

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();//express 앱 구동
+const ejsMate = require('ejs-mate');//ejs-mate라는 템플릿을 사용하겠다
+const path = require('path');
+
+app.engine("ejs", ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));//ejs파일을 사용하는데 default folder -> views
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
-    res.send('Hello World!');
+    res.render('home');
 })
 
 app.listen(3000, ()=>{//
